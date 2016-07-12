096b5c7a590e7cd47c7bd12c3608cf7cb0c11e29
Gregor04 <fredd_d_cruz@outlook.es>
2016-07-12 13:02:50 -0500

    Update calculadora.py
    agregado funcion limpiar, mas libreria math para usar "pi" y numero "e"

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,14 @@
 from tkinter import *
+from math import pi, e
 
 def almacenar(x):
     global operador
     operador=operador+str(x)
+    otrav.set(operador)
+    return otrav
+def limpiar():
+    global operador
+    operador=""
     otrav.set(operador)
     return otrav
 def resultado():
@@ -25,20 +31,20 @@ boton1=Button(ventana,text="7",bd=6,bg="#102E59",fg="white",command=lambda:almac
 boton2=Button(ventana,text="8",bd=6,bg="#102E59",fg="white",command=lambda:almacenar(8),width=5).place(x=100,y=120)
 boton3=Button(ventana,text="9",bd=6,bg="#102E59",fg="white",command=lambda:almacenar(9),width=5).place(x=160,y=120)
 boton4=Button(ventana,text="/",bd=6,bg="#102E59",fg="white",command=lambda:almacenar("/"),width=5).place(x=220,y=120)
-boton5=Button(ventana,text="AC",bd=6,bg="#102E59",fg="white",width=5).place(x=280,y=120)
+boton5=Button(ventana,text="AC",bd=6,bg="#102E59",fg="white",command=limpiar,width=5).place(x=280,y=120)
 boton6=Button(ventana,text="4",bd=6,bg="#102E59",fg="white",command=lambda:almacenar(4),width=5).place(x=40,y=160)
 boton7=Button(ventana,text="5",bd=6,bg="#102E59",fg="white",command=lambda:almacenar(5),width=5).place(x=100,y=160)
 boton8=Button(ventana,text="6",bd=6,bg="#102E59",fg="white",command=lambda:almacenar(6),width=5).place(x=160,y=160)
 boton9=Button(ventana,text="x",bd=6,bg="#102E59",fg="white",command=lambda:almacenar("*"),width=5).place(x=220,y=160)
-boton10=Button(ventana,text="ANS",bd=6,bg="#102E59",fg="white",width=5).place(x=280,y=160)
+boton10=Button(ventana,text="ANS",bd=6,bg="#102E59",fg="white",command=lambda:almacenar(operador),width=5).place(x=280,y=160)
 boton11=Button(ventana,text="1",bd=6,bg="#102E59",fg="white",command=lambda:almacenar(1),width=5).place(x=40,y=200)
 boton12=Button(ventana,text="2",bd=6,bg="#102E59",fg="white",command=lambda:almacenar(2),width=5).place(x=100,y=200)
 boton13=Button(ventana,text="3",bd=6,bg="#102E59",fg="white",command=lambda:almacenar(3),width=5).place(x=160,y=200)
 boton14=Button(ventana,text="+",bd=6,bg="#102E59",fg="white",command=lambda:almacenar("+"),width=5).place(x=220,y=200)
-boton15=Button(ventana,text="pi",bd=6,bg="#102E59",fg="white",width=5).place(x=280,y=200)
+boton15=Button(ventana,text="pi",bd=6,bg="#102E59",fg="white",command=lambda:almacenar(str(pi)),width=5).place(x=280,y=200)
 boton16=Button(ventana,text="0",bd=6,bg="#102E59",fg="white",command=lambda:almacenar(0),width=5).place(x=40,y=240)
 boton17=Button(ventana,text=".",bd=6,bg="#102E59",fg="white",command=lambda:almacenar("."),width=5).place(x=100,y=240)
-boton18=Button(ventana,text="e",bd=6,bg="#102E59",fg="white",width=5).place(x=160,y=240)
+boton18=Button(ventana,text="e",bd=6,bg="#102E59",fg="white",command=lambda:almacenar(str(e)),width=5).place(x=160,y=240)
 boton19=Button(ventana,text="-",bd=6,bg="#102E59",fg="white",command=lambda:almacenar("-"),width=5).place(x=220,y=240)
 boton20=Button(ventana,text="=",bd=6,bg="#102E59",fg="white",command=resultado,width=5).place(x=280,y=240)
 
